9095d2cd6d635162c9c3bec2df0e906ad91d766b
unknown <alan@.(none)>
2010-04-08 12:05:39 +0200

    init

---
+++ js/taskboard.js
@@ -0,0 +1,19 @@
+$(function() {
+	$(".column").sortable({
+		connectWith: '.column'
+	});
+
+	$(".portlet").addClass("ui-widget ui-widget-content ui-helper-clearfix ui-corner-all")
+		.find(".portlet-header")
+			.addClass("ui-widget-header ui-corner-all")
+			.prepend('<span class="ui-icon ui-icon-carat-1-n"></span>')
+			.end()
+		.find(".portlet-content");
+
+	$(".portlet-header .ui-icon").click(function() {
+		$(this).toggleClass("ui-icon-carat-1-n").toggleClass("ui-icon-carat-1-s");
+		$(this).parents(".portlet:first").find(".portlet-content").toggle();
+	});
+
+	$(".column").disableSelection();
+});
