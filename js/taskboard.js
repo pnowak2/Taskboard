beff90b6110146d54d599d965d0c6d07e0ea603c
unknown <alan@.(none)>
2010-04-08 12:17:32 +0200

    ikony

--- js/taskboard.js
+++ js/taskboard.js
@@ -6,6 +6,10 @@ $(function() {
 	$(".portlet").addClass("ui-widget ui-widget-content ui-helper-clearfix ui-corner-all")
 		.find(".portlet-header")
 			.addClass("ui-widget-header ui-corner-all")
+			.prepend('<span class="ui-icon ui-icon-wrench"></span>')
+			.prepend('<span class="ui-icon ui-icon-person"></span>')
+			.prepend('<span class="ui-icon ui-icon-shuffle"></span>')
+			.prepend('<span class="ui-icon ui-icon-calendar"></span>')
 			.prepend('<span class="ui-icon ui-icon-carat-1-n"></span>')
 			.end()
 		.find(".portlet-content");
