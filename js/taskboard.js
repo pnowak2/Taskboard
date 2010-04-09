45e0e36da2d54c3d7c9bf11b483ababa167a0556
pnowak2 <p.nowak2@gmail.com>
2010-04-09 16:10:08 +0200

    poprawki.

--- js/taskboard.js
+++ js/taskboard.js
@@ -8,23 +8,22 @@ $(function() {
 		opacity: 0.7,
 		revert: 100,
 		tolerance: 'pointer',
-		forcePlaceholderSize: true
+		forcePlaceholderSize: true,
+		start: function(event, ui) {
+			  $(ui.placeholder).addClass("ui-corner-all");
+		}
 	});
 
 	$(".portlet").addClass("ui-widget ui-widget-content ui-helper-clearfix ui-corner-all")
 		.find(".portlet-header")
 			.addClass("ui-widget-header ui-corner-all")
-//			.prepend('<span class="ui-icon ui-icon-wrench" title="Edytuj"></span>')
-//			.prepend('<span class="ui-icon ui-icon-person" title="Przypisz do mnie"></span>')
-//			.prepend('<span class="ui-icon ui-icon-transfer-e-w" title="Przypisanie zadania"></span>')
-//			.prepend('<span class="ui-icon ui-icon-clock" title="Rejestracja czasu"></span>')
 			.prepend('<span class="ui-icon ui-icon-carat-1-n" title="Minimalizuj/Maksymalizuj"></span>')
 			.end()
 		.find(".portlet-content");
 
 	$(".portlet-header .ui-icon-carat-1-n").click(function() {
 		$(this).toggleClass("ui-icon-carat-1-n").toggleClass("ui-icon-carat-1-s");
-		$(this).parents(".portlet:first").find(".portlet-content").toggle();
+		$(this).parents(".portlet:first").find(".portlet-content").toggle("fast");
 	});
 	
 	// Show menu when #myDiv is clicked
@@ -34,4 +33,5 @@ $(function() {
 		function(action, el, pos) {
 		alert(action);
 	});
+	
 });
