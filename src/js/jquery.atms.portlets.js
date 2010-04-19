5169daa71cf4964545aeef3037078c6a0e7d732d
pnowak2 <p.nowak2@gmail.com>
2010-04-19 09:18:01 +0200

    dodanie do eventu update informacji o wierszu

--- src/js/jquery.atms.portlets.js
+++ src/js/jquery.atms.portlets.js
@@ -7,7 +7,7 @@
 					   contextMenu: null,
 					   contextMenuAction: function(item, el){},
 					   allCollapsedByDefault: false,
-					   update: function(portlet, newColumn, oldColumn){}
+					   update: function(portlet, newColumn, oldColumn, row){}
 	     }
 	     if (settings) $.extend(config, settings);
 
@@ -73,7 +73,7 @@
 		}
 		
 		function receiveMethod(event, ui){
-			config.update(ui.item, ui.item.parents(".atms-ui-portlet-column:first"), ui.sender);
+			config.update(ui.item, ui.item.parents(".atms-ui-portlet-column:first"), ui.sender, ui.item.parents(".atms-ui-portlet-row:first"));
 		}
 		
 		/*
