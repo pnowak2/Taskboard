43ae9ba5e733f5faf900fef5d63314f333268d56
Piotr Nowak <p.nowak2@gmail.com>
2010-04-28 21:46:22 +0200

    testy

--- src/js/jquery.atms.portlets.js
+++ src/js/jquery.atms.portlets.js
@@ -214,7 +214,7 @@
 		el.find(".atms-ui-portlet-column-header").addClass("ui-corner-all");
 		el.find(".atms-ui-portlet-column-pointer").addClass("ui-state-highlight ui-corner-all");
 		el.find(".atms-ui-portlet-row").addClass("ui-helper-clearfix ui-corner-all ui-widget-content");
-		el.find(".atms-ui-portlet-column .atms-ui-portlet-column-header").addClass("ui-widget-header");
+		//el.find(".atms-ui-portlet-column .atms-ui-portlet-column-header").addClass("ui-widget-header");
 	 	el.find(".atms-ui-portlet").addClass("ui-widget ui-widget-content ui-helper-clearfix ui-corner-all")
 		.find(".atms-ui-portlet-header")
 			.addClass("ui-widget-header ui-corner-all")
