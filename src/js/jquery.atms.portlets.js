6bfcf335e170ae2c316a5af0b0431d6df920dd0d
pnowak2 <p.nowak2@gmail.com>
2010-04-30 15:05:52 +0200

    search robi trima

--- src/js/jquery.atms.portlets.js
+++ src/js/jquery.atms.portlets.js
@@ -114,7 +114,7 @@
 	      $.expr[":"].containsNoCase = function(el, i, m) {
 	         var search = m[3];
 	         if (!search) return false;
-	         return new RegExp(search,"i").test($(el).text());
+	         return new RegExp(search,"i").test($(el).text().replace(/\s+/g,' '));
 	      };  
 		}
 		
